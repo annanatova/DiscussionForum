6823c493f3f6806903154ba38a06070f315104f9
Анна Натова <annanatova97@gmail.com>
2025-08-13 22:33:29 +0300

    Update theme model

--- src/app/models/theme.model.ts
+++ src/app/models/theme.model.ts
@@ -1,9 +1,10 @@
 import { Comment } from "./comment";
+import { User } from "./user.model";
 
 export interface Theme {
     _id: string;
     themeName: string;
-    userId: string;
+    userId: any;
     subscribers: string[];
     created_at: Date;
     comments?: Comment[];
